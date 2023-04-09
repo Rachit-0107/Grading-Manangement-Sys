12c2c3a1da274702b21c2a5430e182a842941cf4
Rachit Agrawal <80877174+Rachit-0107@users.noreply.github.com>
2023-04-09 19:11:34 +0530

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,3 +1,4 @@
+DROP DATABASE Grading_Man_Sys; 
 create schema Grading_Man_Sys; 
 use Grading_Man_Sys; 
 
@@ -8,7 +9,7 @@ create table student (
     sysPwd varchar(30) not null,
     
     primary key(studID)
-); 
+);  
 
 DROP TABLE IF EXISTS `teacher`;
 create table teacher (
@@ -23,8 +24,10 @@ DROP TABLE IF EXISTS `course`;
 create table course (
 	courseName varchar(30) not null,
     courseID int not null unique,
-	courseCreds int not null,
+	courseCreds int not null, 
+    taughtBy int not null,
     
+    foreign key(taughtBy) references teacher(teacID) on delete cascade,
     primary key(courseID)
 ); 
 
@@ -36,17 +39,7 @@ create table courseTaken (
 	foreign key(studID) references student(studID) on delete cascade, 
 	foreign key(courseID) references course(courseID) on delete cascade, 
 	primary key(studID, courseID)
-); 
-
-DROP TABLE IF EXISTS `courseTaught`;
-create table courseTaught (
-	teacID int not null, 
-	courseID int not null, 
-	
-	foreign key(teacID) references teacher(teacID) on delete cascade, 
-	foreign key(courseID) references course(courseID) on delete cascade, 
-	primary key(teacID, courseID)
-); 
+);
 
 DROP TABLE IF EXISTS `grades`;
 create table grades (
@@ -62,7 +55,77 @@ create table grades (
 	foreign key(courseID) references course(courseID) on delete cascade, 
     foreign key(teacID) references teacher(teacID) on delete cascade,
 	primary key(studID,courseID,teacID)
-); 
+);  
+
+INSERT INTO 
+	student(studName, studID, sysPwd)
+VALUES
+	("Atish", 3102, "rahulfan"),
+	("Rishabh", 3104, "avgcoder"),
+	("Nitant", 3112, "kothari0412"),
+	("Poojan", 3113, "poojan123"),
+	("Mann", 3111, "shahman987"),
+	("Nishal", 3110, "nis2504"),
+	("Deepam", 3107, "ddesai123"); 
+
+
+INSERT INTO
+	teacher(teacName, teacID, sysPwd) 
+VALUES
+	("Rahul", 101, "rahul123"),
+	("Raj", 102, "raj123"),
+	("Rajesh", 103, "rajesh123");
+
+
+INSERT INTO
+	course(courseName, courseID, courseCreds, taughtBy) 
+VALUES
+	("Database Systems", 212, 4, 101),
+	("Operating Systems", 372, 3, 102),
+	("Computer Networks", 351, 4, 101),
+	("Data Structures", 211, 4, 102),
+	("Algorithms", 381, 3, 103),
+	("Computer Architecture", 342, 3, 103); 
+
+
+INSERT INTO 
+	courseTaken(studID, courseID)	
+VALUES
+	(3102,212),
+	(3102,372), 
+	(3104,212),
+	(3104,211),
+	(3112,372),
+	(3112,381),
+	(3113,351), 
+	(3111,211),
+	(3111,381),
+	(3110,351),
+	(3110,342),
+	(3107,212),
+	(3107,372),
+	(3107,342),
+	(3111,342); 
+
+
+INSERT INTO 
+	grades(studID, courseID, teacID, assig1, midsem, assig2, endsem, finalGrade)
+VALUES 
+	(3102,212,101,'A','A','A','A','A'),
+	(3102,372,102,'A','A','A','A','B'),
+	(3104,212,101,'A','A','A','A','B'),
+	(3104,211,102,'A','A','A','A','A'),
+	(3112,372,102,'A','A','A','A','C'),
+	(3112,381,103,'A','A','A','A','C'),
+	(3113,351,101,'A','A','A','A','A'),
+	(3111,211,102,'A','A','A','A','A'),
+	(3111,381,103,'A','A','A','A','D'),
+	(3110,351,101,'A','A','A','A','D'),
+	(3110,342,103,'A','A','A','A','E'),
+	(3107,212,101,'A','A','A','A','B'),
+	(3107,372,102,'A','A','A','A','A'),
+	(3107,342,103,'A','A','A','A','C'),
+	(3111,342,103,'A','A','A','A','B');
 
 DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `viewSCourses`(IN tstudID int)
@@ -83,7 +146,7 @@ NOT DETERMINISTIC
 SQL SECURITY INVOKER
 COMMENT 'Input - Student ID , Output - Courses taught by the teacher'
 BEGIN
-	SELECT * FROM course where courseID in (SELECT courseID FROM courseTaught WHERE teacID = tteachID);
+	SELECT * FROM course where taughtBy = tteachID;
 END$$
 
 
@@ -145,7 +208,7 @@ NOT DETERMINISTIC
 SQL SECURITY INVOKER
 COMMENT 'Input -Course ID and Teacher ID, Output - Grades of all students in the particular course taught by the teacher'
 BEGIN
-	IF((SELECT COUNT(*) FROM courseTaught WHERE courseID = tcourseID AND teacID = tteachID) = 0) THEN
+	IF((SELECT COUNT(*) FROM course WHERE courseID = tcourseID AND taughtBy = tteachID) = 0) THEN
 		SELECT 'Teacher is not teaching this course';
 	ELSE
 		SELECT student.studName as 'Student', grades.assig1 as 'Assignment-1', grades.midsem as 'Midsem', grades.assig2 as 'Assignment-2', grades.endsem as 'Endsem' 
@@ -197,7 +260,7 @@ BEGIN
 	IF((SELECT COUNT(*) FROM courseTaken WHERE courseID = tcourseID AND studID = tstudID) = 0) THEN
 		SELECT 'Student is not taking this course';
 	ELSE 
-		IF((SELECT COUNT(*) FROM courseTaught WHERE courseID = tcourseID AND teacID = tteachID) = 0) THEN 
+		IF((SELECT COUNT(*) FROM course WHERE courseID = tcourseID AND taughtBy = tteachID) = 0) THEN 
 			SELECT 'Teacher is not teaching this course';
 		ELSE
 			UPDATE grades SET assig1 = tgrade WHERE courseID = tcourseID AND studID = tstudID AND teacID = tteachID; 
@@ -216,7 +279,7 @@ BEGIN
 	IF((SELECT COUNT(*) FROM courseTaken WHERE courseID = tcourseID AND studID = tstudID) = 0) THEN
 		SELECT 'Student is not taking this course';
 	ELSE 
-		IF((SELECT COUNT(*) FROM courseTaught WHERE courseID = tcourseID AND teacID = tteachID) = 0) THEN 
+		IF((SELECT COUNT(*) FROM course WHERE courseID = tcourseID AND taughtBy = tteachID) = 0) THEN 
 			SELECT 'Teacher is not teaching this course';
 		ELSE
 			UPDATE grades SET assig2 = tgrade WHERE courseID = tcourseID AND studID = tstudID AND teacID = tteachID;
@@ -235,7 +298,7 @@ BEGIN
 	IF((SELECT COUNT(*) FROM courseTaken WHERE courseID = tcourseID AND studID = tstudID) = 0) THEN
 		SELECT 'Student is not taking this course';
 	ELSE 
-		IF((SELECT COUNT(*) FROM courseTaught WHERE courseID = tcourseID AND teacID = tteachID) = 0) THEN 
+		IF((SELECT COUNT(*) FROM course WHERE courseID = tcourseID AND taughtBy = tteachID) = 0) THEN 
 			SELECT 'Teacher is not teaching this course';
 		ELSE
 			UPDATE grades SET midsem = tgrade WHERE courseID = tcourseID AND studID = tstudID AND teacID = tteachID;
@@ -254,7 +317,7 @@ BEGIN
 	IF((SELECT COUNT(*) FROM courseTaken WHERE courseID = tcourseID AND studID = tstudID) = 0) THEN
 		SELECT 'Student is not taking this course';
 	ELSE 
-		IF((SELECT COUNT(*) FROM courseTaught WHERE courseID = tcourseID AND teacID = tteachID) = 0) THEN 
+		IF((SELECT COUNT(*) FROM course WHERE courseID = tcourseID AND taughtBy = tteachID) = 0) THEN 
 			SELECT 'Teacher is not teaching this course';
 		ELSE
 			UPDATE grades SET endsem = tgrade WHERE courseID = tcourseID AND studID = tstudID AND teacID = tteachID;
@@ -273,7 +336,7 @@ BEGIN
 	IF((SELECT COUNT(*) FROM courseTaken WHERE courseID = tcourseID AND studID = tstudID) = 0) THEN
 		SELECT 'Student is not taking this course';
 	ELSE 
-		IF((SELECT COUNT(*) FROM courseTaught WHERE courseID = tcourseID AND teacID = tteachID) = 0) THEN 
+		IF((SELECT COUNT(*) FROM course WHERE courseID = tcourseID AND taughtBy = tteachID) = 0) THEN 
 			SELECT 'Teacher is not teaching this course';
 		ELSE
 			UPDATE grades SET finalGrade = tgrade WHERE courseID = tcourseID AND studID = tstudID AND teacID = tteachID;
@@ -320,7 +383,7 @@ BEGIN
 	IF((SELECT COUNT(*) FROM student WHERE studID = tstudID) = 0) THEN
 		INSERT INTO student VALUES (tstudID, tname, tpwd);
 	ELSE
-		SELECT 'Student already exists';
+		SELECT 'StudentID already exists';
 	END IF;
 END$$ 
 
@@ -335,7 +398,7 @@ BEGIN
 	IF((SELECT COUNT(*) FROM teacher WHERE teacID = tteacID) = 0) THEN
 		INSERT INTO teacher VALUES (tteacID, tname, tpwd);
 	ELSE
-		SELECT 'Teacher already exists';
+		SELECT 'TeacherID already exists';
 	END IF;
 END$$ 
 
@@ -396,7 +459,7 @@ NOT DETERMINISTIC
 SQL SECURITY INVOKER
 COMMENT 'Input - Course ID , Output - Lowest grade of the course'
 BEGIN
-	IF((SELECT COUNT(*) FROM courseTaught WHERE courseID = tcourseID AND teacID = tteachID) = 0) THEN
+	IF((SELECT COUNT(*) FROM course WHERE courseID = tcourseID AND taughtBy = tteachID) = 0) THEN
 		SELECT 'Teacher is not teaching this course';
 	ELSE
 		SELECT max(finalGrade) 
